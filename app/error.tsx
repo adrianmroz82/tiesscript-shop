afec6e94a5ebf9ef206c03ff17bdbab393bcf1dc
Adrian <adrianmroz82@gmail.com>
2024-02-27 20:05:37 +0100

    handle firebase connection

---
+++ app/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main>
+      <h1>404 - Page Not Found</h1>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1 className={styles.description}>Home</h1>
+      <Link href="/about">About</Link>
     </main>
   );
 }
