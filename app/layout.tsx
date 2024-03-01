f75be1cefe127b7457e0df2563c01f3edb11f572
Adrian Mróz <hlepzje@gmail.com>
2024-03-01 22:41:03 +0100

    add toast to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Header } from "@/components/header";
+import { Toaster } from "@/components/shadcn-ui/toaster";
+import { ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +16,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -24,6 +26,7 @@ export default function RootLayout({
           <Header />
           {children}
           {/* <Footer /> */}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
