638b68d1e757ab83b450d4a43e3030206f66bb8b
Adrian Mróz <hlepzje@gmail.com>
2024-03-03 11:10:25 +0100

    add theme provider

---
+++ src/components/theme-switcher.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Switch } from "./shadcn-ui/switch";
+
+export function ThemeSwitcher() {
+  const { setTheme } = useTheme();
+  const currentTheme = useTheme().theme;
+
+  const handleTheme = () => {
+    setTheme(currentTheme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <div className="flex items-center space-x-2">
+      <Switch onClick={handleTheme} id="airplane-mode" />
+    </div>
+  );
+}
