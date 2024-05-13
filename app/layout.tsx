0efd6c0df83eb41dd8fc8bcabcf41e4db0bf89f1
Adrian <adrianmroz82@gmail.com>
2024-05-13 23:19:43 +0200

    add RootLayout Sidebar and flex

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,17 @@
 "use client";
 
 import { ReactNode } from "react";
+import localFont from "next/font/local";
 
 import { ThemeProvider } from "@/components/theme-provider";
 import { Header } from "@/components/header";
 import { Toaster } from "@/components/shadcn-ui/toaster";
 import { AuthProvider } from "./context/auth-provider";
+import { Sidebar } from "@/components/sidebar";
 
 import "./globals.css";
 
-// export const metadata: Metadata = {
-//   title: "Tiesscript",
-//   description: "Vintage ties shop",
-// };
+const satoshiFont = localFont({ src: "../public/fonts/satoshi-light.otf" });
 
 export default function RootLayout({
   children,
@@ -20,13 +19,18 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={`${satoshiFont.className}`}>
       <head />
-      <body>
+      <body className="flex">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <AuthProvider>
-            <Header />
-            {children}
+            <div className="flex flex-col w-full h-full">
+              <Header />
+              <div className="flex flex-row w-full h-full">
+                <Sidebar />
+                <main className="w-full h-full">{children}</main>
+              </div>
+            </div>
             <Toaster />
           </AuthProvider>
         </ThemeProvider>
