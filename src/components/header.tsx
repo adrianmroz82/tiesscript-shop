81499917fe34fa408a0a47d41c33586ab025c82a
Adrian <adrianmroz82@gmail.com>
2024-05-20 21:25:05 +0200

    add details page info

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,14 +3,24 @@
 import Link from "next/link";
 import { ThemeSwitcher } from "./theme-switcher";
 
+// function Block() {
+//   return (
+//     <div className="bg-gray-200 shadow-lg w-[250px] h-[8rem]">
+//       <Link className="hover:underline" href="/">
+//         Home
+//       </Link>
+//     </div>
+//   );
+// }
+
 export function Header() {
   return (
-    <header className="bg-dark-500 p-6 shadow-lg rounded-lg">
-      <nav className="flex items-center justify-between ">
-        <Link className="font-bold text-m" href="/">
+    <header className="bg-dark-500 p-6 shadow-lg rounded-lg h-[6rem]">
+      <nav className="flex items-center justify-between">
+        <Link className="font-bold text-xl" href="/">
           Logo
         </Link>
-        <div className="flex space-x-12 text-l font-bold justify-between">
+        <div className="flex space-x-16 text-xl font-bold justify-between">
           <Link className="hover:underline" href="/">
             Home
           </Link>
---
+++ src/components/product-details-info.tsx
@@ -0,0 +1,31 @@
+// import Image from "next/image";
+import { Product } from "../../app/models/product.model";
+import { Button } from "./shadcn-ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./shadcn-ui/card";
+// import ShoppingCartIcon from "../assets/cart-icon.svg";
+
+interface Props {
+  product: Product;
+}
+
+export function ProductDetailsInfo({ product }: Props) {
+  const { name, price, length, width } = product;
+
+  return (
+    <>
+      <Card className="shadow-lg m-6 p-8 w-full">
+        <CardHeader className="pb-4">
+          <CardTitle className="text-3xl">{name}</CardTitle>
+          <CardDescription className="text-xl text-gray-500">This is a short description</CardDescription>
+        </CardHeader>
+        <CardContent className="my-4">
+          <h4 className="font-semibold text-2xl">Price: {price} zł</h4>
+          <p className="text-md text-gray-500 mt-2">Length: {length} cm</p>
+          <p className="text-md text-gray-500 mt-2">Width: {width} cm</p>
+          <Button className="mt-8 w-1/4 bg-[#5009DC] text-md font-bold ">Add to cart</Button>
+          {/* <Image className="fill-white" src={ShoppingCartIcon} alt="Add to cart" width={24} height={24} /> */}
+        </CardContent>
+      </Card>
+    </>
+  );
+}
