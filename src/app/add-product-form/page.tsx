990449028fab5fa753258fe7687afa8b92ec0832
Adrian <adrianmroz82@gmail.com>
2024-08-29 23:18:36 +0200

    change product form url

--- src/app/add-product-form/page.tsx
+++ src/app/add-product-form/page.tsx
@@ -14,7 +14,7 @@ import { db } from "@/firebase/firebase";
 import { getAllCategories } from "@/lib/api/getAllCategories";
 import { Category, ProductWithImages } from "@/models/product.model";
 
-export default function ProductForm() {
+export default function AddProductForm() {
   const { toast } = useToast();
   const [formData, setFormData] = useState<ProductWithImages>({
     id: "",
