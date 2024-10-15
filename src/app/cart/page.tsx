e53753b63c5315a718b35a8e387c89241b683904
Adrian <adrianmroz82@gmail.com>
2024-10-15 20:57:45 +0200

    add cart components

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { CartItem } from "@/components/cart-item";
+import { EmptyCart } from "@/components/empty-cart";
+import { useAppSelector } from "@/store/utils/redux-hooks";
+
+export default function Cart() {
+  const cart = useAppSelector((state) => state.cart);
+  console.log(cart);
+
+  const totalPrice = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
+  return (
+    <>
+      <div className="p-8 max-w-4xl mx-auto bg-white mt-8 rounded-xl">
+        <h3>Cart</h3>
+        {cart.length === 0 ? (
+          <EmptyCart />
+        ) : (
+          <ul className="list-none p-0">
+            {cart.map((cartItem) => (
+              <CartItem key={cartItem.id} cartItem={cartItem} />
+            ))}
+            <div className="text-right font-bold mt-8">Total Price: ${totalPrice.toFixed(2)}</div>
+          </ul>
+        )}
+      </div>
+    </>
+  );
+}
---
+++ src/components/cart-item.tsx
@@ -0,0 +1,50 @@
+import { X as RemoveIcon } from "lucide-react";
+import Image from "next/image";
+
+// import { CartItem as CartItemProps } from "@/lib/model";
+import { useCartActions } from "@/lib/hooks/useCartActions";
+
+interface Props {
+  cartItem: any;
+}
+
+// TODO: fix types, price.toFixed bug, removeIcon alignment to right doesnt work
+export function CartItem({ cartItem }: Props) {
+  const { handleIncrementQuantity, handleDecrementQuantity, handleRemoveItem } = useCartActions();
+  const { id, name, images, price, quantity } = cartItem;
+
+  return (
+    <li className="flex items-center mb-5 pb-5 border-b-2">
+      <Image priority className="rounded" src={images[0]} alt={name} width={80} height={80} />
+      <div className="flex-1 ml-8">
+        <h3 className="mb-2.5">{name}</h3>
+        <div className="flex items-center">
+          <span>Quantity:</span>
+          <button
+            data-testid="cart-item-decrement-button"
+            className="mx-2 cursor-pointer border-none bg-slate-200 py-1 px-2"
+            onClick={() => handleDecrementQuantity(id)}>
+            -
+          </button>
+          <span>{quantity}</span>
+          <button
+            data-testid="cart-item-increment-button"
+            className="mx-2 cursor-pointer border-none bg-slate-200 py-1 px-2"
+            onClick={() => handleIncrementQuantity(id)}>
+            +
+          </button>
+        </div>
+      </div>
+      <div className="ml-5 text-right">
+        <RemoveIcon
+          data-testid="remove-icon"
+          className="cursor-pointer"
+          size={24}
+          onClick={() => handleRemoveItem(id)}
+        />
+        <p>${price.toFixed(2)} each</p>
+        <p>Total: ${(price * quantity).toFixed(2)}</p>
+      </div>
+    </li>
+  );
+}
