e44628822217d5a91f43afc99700210aab1cb328
Adrian <adrianmroz82@gmail.com>
2025-01-01 23:20:55 +0100

    add order data table

--- src/app/(client)/cart/page.tsx
+++ src/app/(client)/cart/page.tsx
@@ -2,11 +2,32 @@
 
 import { CartItem } from "@/components/cart-item";
 import { EmptyCart } from "@/components/empty-cart";
+import { Button } from "@/components/shadcn-ui/button";
+import { createOrder } from "@/lib/api/createOrder";
+import { PaymentMethod } from "@/models/order.model";
 import { useAppSelector } from "@/store/utils/redux-hooks";
 
 export default function Cart() {
   const cart = useAppSelector((state) => state.cart);
   const totalPrice = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
+  const handleCreateOrder = async () => {
+    console.log(cart);
+
+    const paymentMethod: PaymentMethod = "PayPal";
+    const paymentAmount = totalPrice;
+
+    // TODO: handle plural number of products
+    const concatenatedIds = cart.map((item) => item.id).join(",");
+    const concatenatedNames = cart.map((item) => item.name).join(", ");
+
+    await createOrder({
+      id: concatenatedIds,
+      productName: concatenatedNames,
+      paymentAmount,
+      paymentMethod,
+    });
+  };
 
   return (
     <div className="p-8 max-w-4xl mx-auto bg-white mt-8 rounded-xl">
@@ -21,6 +42,9 @@ export default function Cart() {
           <div className="text-right font-bold mt-8">Total Price: ${totalPrice.toFixed(2)}</div>
         </ul>
       )}
+      <Button className="mt-8" variant="outline" onClick={handleCreateOrder}>
+        Mock Order
+      </Button>
     </div>
   );
 }
