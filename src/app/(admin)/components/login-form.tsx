e592c41289847832f4ef5689040399bbaea0d2f4
Adrian <adrianmroz82@gmail.com>
2025-02-24 21:52:43 +0100

    setup supabase & add supabase auth

---
+++ src/app/(admin)/components/login-form.tsx
@@ -0,0 +1,38 @@
+import { login } from "@/app/login/actions";
+import { Button } from "@/components/shadcn-ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/shadcn-ui/card";
+import { Input } from "@/components/shadcn-ui/input";
+import { Label } from "@/components/shadcn-ui/label";
+import { cn } from "@/lib/utils";
+
+export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>Enter your email below to login to your account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" name="email" placeholder="m@example.com" required />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" name="password" required />
+              </div>
+              <Button formAction={login} type="submit" className="w-full">
+                Login
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
