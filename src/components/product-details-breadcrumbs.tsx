106dc16a522ebed2f39d5a0f3bbd14ccaf849e6f
Adrian <adrianmroz82@gmail.com>
2025-04-20 21:09:04 +0200

    add breadcrumbs for details page

---
+++ src/components/product-details-breadcrumbs.tsx
@@ -0,0 +1,39 @@
+// "use client";
+
+// import { useParams } from "next/navigation";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/shadcn-ui/breadcrumb";
+import { capitalize } from "@/lib/utils/capitalize";
+
+interface Props {
+  product: Product;
+}
+
+export function ProductDetailsBreadcrumbs({ product }: Props) {
+  const { category, name } = product;
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">All products</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href={`/${category}`}>{capitalize(category!)}</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>{capitalize(name)}</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
