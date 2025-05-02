c870b16557c67452df85ed4fe693ff712ad30298
Adrian <adrianmroz82@gmail.com>
2025-05-02 20:44:10 +0200

    fix middleware

--- src/lib/global.d.ts
+++ src/lib/global.d.ts
@@ -5,4 +5,7 @@ declare global {
   type Category = Database["public"]["Tables"]["categories"]["Row"];
   type Product = Database["public"]["Tables"]["products"]["Row"];
   type Order = Database["public"]["Tables"]["orders"]["Row"];
+  type Resource = Database["public"]["Tables"]["resources"]["Row"];
+
+  type ResourceImageUrl = Resource["images"][number];
 }
--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -2,40 +2,39 @@ import { createServerClient } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
 export async function updateSession(request: NextRequest) {
-  let supabaseResponse = NextResponse.next({
-    request,
-  });
+  let supabaseResponse = NextResponse.next({ request });
+
+  const pathname = request.nextUrl.pathname;
+  const isProtectedRoute = pathname.startsWith("/admin");
+
+  if (!isProtectedRoute) {
+    return supabaseResponse;
+  }
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() {
-          return request.cookies.getAll();
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
-          supabaseResponse = NextResponse.next({
-            request,
+        getAll: () => request.cookies.getAll(),
+        setAll: (cookiesToSet) => {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            supabaseResponse.cookies.set(name, value, options);
           });
-          cookiesToSet.forEach(({ name, value, options }) => supabaseResponse.cookies.set(name, value, options));
         },
       },
     }
   );
 
   const {
-    data: { user: _user },
+    data: { user },
   } = await supabase.auth.getUser();
 
-  //  TODO: uncomment after middleware fix
-  // if (!user && !request.nextUrl.pathname.startsWith("/login") && !request.nextUrl.pathname.startsWith("/auth")) {
-  //   // no user, potentially respond by redirecting the user to the login page
-  //   const url = request.nextUrl.clone();
-  //   url.pathname = "/login";
-  //   return NextResponse.redirect(url);
-  // }
+  if (!user) {
+    const loginUrl = request.nextUrl.clone();
+    loginUrl.pathname = "/login";
+    return NextResponse.redirect(loginUrl);
+  }
 
   return supabaseResponse;
 }
